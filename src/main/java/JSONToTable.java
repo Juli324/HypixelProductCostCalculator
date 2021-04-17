0a1816714cbd1d44043ed7324a5f56fcc08c024f
Juli_324 <juja2769@yahoo.de>
2021-04-17 16:41:34 +0200

    Start at JSONToTable

--- src/main/java/JSONToTable.java
+++ src/main/java/JSONToTable.java
@@ -1,3 +1,18 @@
-public Table<> JSONToTable {
-    return table;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+
+public class JSONToTable {
+    TableModel dataModel = new AbstractTableModel() {
+        public int getColumnCount() {
+            return 10;
+        }
+        public int getRowCount() {
+            return 10;
+        }
+        public Object getValueAt(int row, int col) {
+            return new Integer(row * col);
+        }
+    };
+    JTable table = new JTable(dataModel);
+        return table;
 }
