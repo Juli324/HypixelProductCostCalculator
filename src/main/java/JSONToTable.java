0a062ffafd89a3604f219613e2b45b854a43c5f3
Juli_324 <juja2769@yahoo.de>
2021-04-17 16:55:54 +0200

    Start at JSONToTable

--- src/main/java/JSONToTable.java
+++ src/main/java/JSONToTable.java
@@ -1,18 +1,4 @@
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableModel;
-
 public class JSONToTable {
-    TableModel dataModel = new AbstractTableModel() {
-        public int getColumnCount() {
-            return 10;
-        }
-        public int getRowCount() {
-            return 10;
-        }
-        public Object getValueAt(int row, int col) {
-            return new Integer(row * col);
-        }
-    };
-    JTable table = new JTable(dataModel);
-        return table;
+    BiHashMap map = new BiHashMap();
+    map.put
 }
