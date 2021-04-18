6344e3d3331aa2f2bbb2abda14bfd37c36752d8e
Justus <Justus.hollek@gmail.com>
2021-04-18 14:22:20 +0200

    Programmed ItemIndexer.java and tested it in Main

--- src/main/java/ItemIndexer.java
+++ src/main/java/ItemIndexer.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 public class ItemIndexer {
     public static HashMap<String, Integer> create(ArrayList<BazaarItem> arrayList) {
         HashMap<String, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < ; i++) {
-
+        for (int i = 0; i < arrayList.size(); i++) {
+            BazaarItem bzi = arrayList.get(i);
+            hashMap.put(bzi.productId,i);
         }
         return hashMap;
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<BazaarItem> bi = JSONLoader.getBazaarList(JSONLoader.getBazaarJSON());
-        System.out.println(bi.get(100).buyPrice);
+        //System.out.println(bi.get(100).buyPrice);
+        System.out.println(ItemIndexer.create(bi));
     }
 }
