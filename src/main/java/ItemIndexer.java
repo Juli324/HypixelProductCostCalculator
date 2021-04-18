84cc5eaa73a2567c13b75709fa810aa341ea3742
Juli_324 <juja2769@yahoo.de>
2021-04-18 13:36:31 +0200

    Started ItemIndexer

---
+++ src/main/java/ItemIndexer.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ItemIndexer {
+    public static HashMap<String, Integer> create(ArrayList<BazaarItem> arrayList) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < ; i++) {
+
+        }
+        return hashMap;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<BazaarItem> bi = JSONLoader.getBazaarList(JSONLoader.getBazaarJSON());
-        System.out.println(bi.get(5).productId);
+        System.out.println(bi.get(100).buyPrice);
     }
 }
