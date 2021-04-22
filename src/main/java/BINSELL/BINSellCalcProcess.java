82243107712cb1db992384fb6d56adb418774d3c
Adimmortales <Talkanerer@gmail.com>
2021-04-22 14:16:56 +0200

    started BINSellCalcProcess.java

--- src/main/java/BINSELL/BINSellCalcProcess.java
+++ src/main/java/BINSELL/BINSellCalcProcess.java
@@ -11,12 +11,27 @@ public class BINSellCalcProcess {
 
     /**
      * TODO: get ItemID values from bazzar.json and calculate Average
-     *  all Values from wished Item / Number of all prices
+     * all Values from wished Item / Number of all prices
      */
-    public float getAverageValue() {
-        float value = 0;
 
-        return value;
+    public float sumOfPrices() {
+        // TODO:get Item PRices and list them
+        float[] allPrices = new float[0];
+        float price = 0;
+        for (int i = 0; i < allPrices.length; i++) {
+            price = price + allPrices[i];
+        }
+        return price;
+    }
+
+    public float countItems() {
+        // TODO:get Items by ID and append to IntArray
+        int[] Items = new int[0];
+        return Items.length;
+    }
+
+    public float getAverageValue() {
+        return countItems()/sumOfPrices();
     }
 
 }
